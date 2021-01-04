808a039304940c1ad54e01a2ce8cda620212d613
Sultan Karabekov <sulik23@gmail.com>
2021-01-04 13:45:00 -0500

    latest

--- src/app.js
+++ src/app.js
@@ -47,13 +47,6 @@ const App = () => {
     return (
         <div>
           <div className={classes.logoContainer}>
-            {newsArticles.length ? (
-              <div className={classes.infoContainer}>
-                <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
-                <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
-              </div>
-            ) : null}
-            
           </div>
           <NewsCards articles={newsArticles} activeArticle={activeArticle} />
           
